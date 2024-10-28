7111647989279bf38f81ed823ee17764259f23a3
AdrianArceGomez <adrian.arce.gomez@est.una.ac.cr>
2024-10-28 13:13:28 -0600

    the first progress of the proyec

--- Proyecto2PrograAdrianArce,ArweenGranados/Proyecto2PrograAdrianArce.cpp
+++ Proyecto2PrograAdrianArce,ArweenGranados/Proyecto2PrograAdrianArce.cpp
@@ -1,18 +1,19 @@
-
+#include <iostream>
+#include <string>
 #include <SFML/Graphics.hpp>
 using namespace std;
 
-int main() {
-    
+
+int imagen() {
     sf::RenderWindow window(sf::VideoMode(842, 660), "Stardew Valley Map");
 
-    
+
     sf::Texture mapaTexture;
     if (!mapaTexture.loadFromFile("stardewvalleymap.png")) {
-        return -1; 
+        return -1;
     }
 
-    
+
     sf::Sprite mapaSprite;
     mapaSprite.setTexture(mapaTexture);
 
@@ -34,6 +35,51 @@ int main() {
         // Mostrar la ventana actualizada
         window.display();
     }
+
+
+
+}
+
+void menu() {
+    string ruta;
+    int opcion;
+
+    do {
+        cout << "----- bienvenidos al menu porfavor digite una opcion -----\n";
+        cout << "1. Insertar una ruta\n";
+        cout << "2. Modificar una ruta\n";
+        cout << "3. ver mapa\n";
+        cout << "0. Salir\n";
+
+        cin >> opcion;
+
+        if (opcion == 1) {
+            cout << "Ingrese la ruta a insertar: \n";
+        
+        }
+        if (opcion == 2) {
+            cout << "escoja la ruta a modificar\n";
+        }
+        if (opcion == 3) {
+            imagen();
+        }
+            
+        if (opcion == 0) {
+            cout << "Saliendo del programa...\n";
+        
+   
+        }
+
+    } while (opcion != 0);
+}
+
+
+int main() {
+    
+
+    menu();
+
+   
 
     return 0;
 }
